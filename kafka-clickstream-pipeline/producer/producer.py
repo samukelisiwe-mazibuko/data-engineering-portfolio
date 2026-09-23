02392892a49e8180f5e1f3a3a62749ea888b0d8c
samazjhb025 <samazjhb025@student.wethinkcode.co.za>
2026-09-23 22:54:55 +0200

    Generate simulated clickstream events

--- kafka-clickstream-pipeline/producer/producer.py
+++ kafka-clickstream-pipeline/producer/producer.py
@@ -1,4 +1,24 @@
+import random
+import time
+from datetime import datetime
 
-
+pages = [
+    "/",
+    "/products",
+    "/about",
+    "/contact",
+    "/login"
+]
 
 print("Clickstream producer started")
+
+while True:
+    event = {
+        "user_id": random.randint(1, 100),
+        "page": random.choice(pages),
+        "timestamp": datetime.now().isoformat()
+    }
+
+    print(event)
+
+    time.sleep(2)
